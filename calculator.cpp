450992b864c1aa30b50fbe11bc5aa0e4fbfd9337
Hasaan Developer <107426718+muhammed-hasaan@users.noreply.github.com>
2023-10-12 00:37:46 +0500

    C++ Calculator
    C++ Calculator

---
+++ calculator.cpp
@@ -0,0 +1,22 @@
+# include <iostream>;
+using namespace std;
+
+int main() {
+	int num1 ;
+	cout << "Please type first number";
+	cin >> num1;
+	
+	int num2 ;
+	cout << "Please type first number";
+	cin >> num2;
+	
+	cout << "\n";
+	cout << "if you will do plus...." << num1 + num2;
+	cout << "\n" ;
+	cout << "if you will do minus...." << num1 - num2;
+	cout << "\n";
+	cout << "if you will do multiply...." << num1 * num2;
+	cout << "\n";
+	cout << "if you will do divide...." << num1 / num2;
+	
+}
